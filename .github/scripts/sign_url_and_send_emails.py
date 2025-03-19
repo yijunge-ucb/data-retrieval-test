7e83d5b40b5d4203fbc0d48e5decd8e62ca201b6
yijun <yijunge@berkeley.edu>
2025-03-19 16:49:44 -0700

    refactor

--- .github/scripts/sign_url_and_send_emails.py
+++ .github/scripts/sign_url_and_send_emails.py
@@ -1,7 +1,6 @@
 import os
 import base64
 import json
-import time
 import pickle
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -10,6 +9,13 @@ from google.auth.transport.requests import Request
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import base64
+
+
+from google.auth import credentials
+from google.cloud import storage
+from google.oauth2 import service_account
+from google.auth.exceptions import DefaultCredentialsError
+import datetime
 
 
 def decode_and_save_token(encoded_token, output_file_path):
@@ -84,7 +90,59 @@ def send_email(service, sender, to, subject, body):
     except HttpError as error:
         print(f'An error occurred: {error}')
 
+def generate_signed_url(bucket_name, object_name):
+    try:
+        # Get the service account JSON from the GitHub secret (as environment variable)
+        service_account_json = os.getenv('GCP_SERVICE_ACCOUNT_KEY')
+
+        if not service_account_json:
+            raise ValueError("Service account key is not set in environment variables.")
+
+        # Parse the JSON from the environment variable
+        credentials_info = json.loads(service_account_json)
+
+        # Load the credentials from the parsed JSON
+        credentials = service_account.Credentials.from_service_account_info(credentials_info)
+
+        # Initialize Google Cloud Storage client with the loaded credentials
+        storage_client = storage.Client(credentials=credentials, project=credentials.project_id)
+
+        # Get the bucket
+        bucket = storage_client.bucket(bucket_name)
+
+        # Get the blob (file in GCS)
+        blob = bucket.blob(object_name)
+        # Generate a signed URL for the object
+        url = blob.generate_signed_url(
+            expiration=datetime.timedelta(days=7),  # Set the expiration time (e.g., 7 days)
+            method='GET',  # Specify the HTTP method (e.g., GET for download)
+        )
+        
+        return url
+        
+
+    except DefaultCredentialsError as e:
+        print(f"Error: Unable to authenticate with Google Cloud. {e}")
+        return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
 if __name__ == '__main__':
+    # Sign URLs
+    extracted_link = os.getenv('EXTRACTED_LINK')
+    all_links = extracted_link.split(',')
+    results = []
+    for link in all_links:
+        # Your bucket and object details
+        bucket_name = link.split('//')[1].split('/')[0]
+        object_name = '/'.join(link.split('//')[1].split('/')[1:])
+
+        # Generate signed URL
+        url = generate_signed_url(bucket_name, object_name)
+        results.append(url)
+
+
     # Authenticate and build the Gmail API service
     service = authenticate_gmail_api()
     issue_url = os.getenv('ISSUE_URL')
@@ -93,12 +151,11 @@ if __name__ == '__main__':
         sender = 'datahub-dataretrieval@berkeley.edu'  # Replace with your email address
         recipient = os.getenv('RECEIVER_EMAIL')  # Replace with recipient's email
         subject = 'Your Request for Retrieving Old Files'
-        signed_urls = os.getenv("SIGNEDURL", "").strip()
-
+        
         # Format the message
         body = f"\n In response to your request [{issue_url}], please find the URL(s) to the files you requested:\n\n"
-        url_list = signed_urls.split(",")  # Split URLs by comma
-        for url in url_list:
+     
+        for url in results:
             body += f"{url}\n\n"
 
         # Send the email
